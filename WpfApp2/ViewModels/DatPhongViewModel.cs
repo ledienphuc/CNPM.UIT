8db483eb8016b6c94ec71a6ca928edd4cd353ca0
luantranminh <15520454@gm.uit.edu.vn>
2017-05-28 13:55:14 +0700

    Update DatPhongView

---
+++ WpfApp2/ViewModels/DatPhongViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.ViewModels
+{
+    class DatPhongViewModel: ViewModelBase
+    {
+        public ObservableCollection<PhongViewModel> DanhMucPhong { get; set; }
+        private async void LoadData()
+        {
+            ObservableCollection<PhongViewModel> _danhSachPhong = new ObservableCollection<PhongViewModel>();
+            using (var db = new KhachSanContext())
+            {
+                var danhMucPhong = await (from p in db.PHONGs
+                                          join lp in db.LOAIPHONGs on p.MALOAIPHONG equals lp.MALOAIPHONG
+                                          orderby p.MAPHONG
+                                          select new { p.TENPHONG, p.LOAIPHONG, lp.TENLOAIPHONG, lp.DONGIA, p.TINHTRANG, p.GHICHU }).ToListAsync();
+                foreach (var phong in danhMucPhong)
+                {
+                    PhongViewModel phongViewModel = new PhongViewModel { Phong = new PHONG { TENPHONG = phong.TENPHONG, LOAIPHONG = phong.LOAIPHONG, GHICHU = phong.GHICHU, TINHTRANG = phong.TINHTRANG } };
+                    _danhSachPhong.Add(phongViewModel);
+                }
+                DanhMucPhong = _danhSachPhong;
+                RaisePropertyChanged("DanhMucPhong");
+            }
+
+        }
+        public DatPhongViewModel()
+        {
+            LoadData();
+        }
+    }
+}
